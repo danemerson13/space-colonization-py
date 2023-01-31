5aa6f345507cc8e08223ede9021bd1606494c635
Dan Emerson <danielem@andrew.cmu.edu>
2023-01-31 10:44:10 -0500

    Adding cone generating function to plotter

--- util/plotter.py
+++ util/plotter.py
@@ -18,6 +18,66 @@ def plotByBranch(col):
 
 ##### Helper Functions #####
 
+def createConeAdv(baseCenter, topCenter, baseRadius, topRadius, baseAxis, topAxis, stackCount, sliceCount):
+    # Functionally the same as createCone() but also accounts for the normal direction 
+    # of each cone face and interpolates intermediate stack normals
+
+    # Check slice and stack count is 2 or more
+    if sliceCount < 2:
+        raise ValueError("Slice count must be 2 or higher")
+    if stackCount < 2:
+        raise ValueError("Stack count must be 2 or higher")
+
+    V = list()
+    F = list()
+
+    axis = topCenter - baseCenter
+    coneLength = np.linalg.norm(axis)
+    axis = axis/coneLength
+    
+    dTheta = 2 * np.pi / sliceCount
+    # Normalize base and top axes
+    baseAxis = baseAxis/np.linalg.norm(baseAxis)
+    topAxis = topAxis/np.linalg.norm(topAxis)
+    # Make sure random ray is orthogonal to the two axes
+    randomRay = np.cross(baseAxis,topAxis)
+
+    # Make the vertices
+    for i in range(stackCount):
+        # Compute center of the stack
+        center = baseCenter + i/stackCount * coneLength * axis
+        # Interpolate the radius based on stack
+        stackRadius = baseRadius + (topRadius - baseRadius) * i/(stackCount-1)
+        print(stackRadius)
+        # Determine stack axis by interpolating between base and stack axes
+        stackAxis = (1 - i/(stackCount-1)) * baseAxis + i/(stackCount-1) * topAxis
+        stackAxis = stackAxis/np.linalg.norm(stackAxis)
+        print(stackAxis)
+        # Rotate around stack generating vertex for each slice
+        for j in range(sliceCount):
+            # TODO inconsistent rotation of face because of np.cross(np.cross())
+            V.append(center + stackRadius * (np.cos(dTheta*j)*np.cross(stackAxis,randomRay) + np.sin(dTheta*j)*np.cross(np.cross(stackAxis,randomRay),axis)))
+            # Assign the indices
+            if i < stackCount-1 and j < sliceCount-1:
+                # F.append([sliceCount*i+j, sliceCount*i+j+1, sliceCount*(i+1)+j+1])
+                # F.append([sliceCount*i+j, sliceCount*(i+1)+j+1, sliceCount*(i+1)+j])
+                F.append([sliceCount*i+j, sliceCount*(i+1)+j+1, sliceCount*i+j+1])
+                F.append([sliceCount*i+j, sliceCount*(i+1)+j, sliceCount*(i+1)+j+1])
+        if i < stackCount - 1:
+            # F.append([sliceCount*(i+1)-1, sliceCount*i, sliceCount*(i+1)])
+            # F.append([sliceCount*(i+1)-1, sliceCount*(i+1), sliceCount*(i+2)-1])
+            F.append([sliceCount*(i+1)-1, sliceCount*(i+1), sliceCount*i])
+            F.append([sliceCount*(i+1)-1, sliceCount*(i+2)-1, sliceCount*(i+1)])
+
+    # Make the mesh
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(V)
+    mesh.triangles = o3d.utility.Vector3iVector(F)
+    mesh.compute_triangle_normals()
+    mesh.compute_vertex_normals()
+
+    return mesh
+
 def createCylinder(a, b, radius, color):
     # Make template cylinder aligned with z-axis
     height = np.linalg.norm(b-a)
