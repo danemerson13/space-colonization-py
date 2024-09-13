9d6d7f4d29fe16701bd121aad792e0d340b6716a
Dan Emerson <danielem@andrew.cmu.edu>
2024-09-13 13:03:51 -0400

    Adding file to perform convergence study on dt

---
+++ tConvergence.py
@@ -0,0 +1,30 @@
+import pickle
+import time
+import os
+
+from src import colony
+
+def main():
+    dts = [1e1, 5e0, 1e0, 5e-1, 1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4]
+    filename = 'results/100SL/sample0/model.pkl'
+    for dt in range(dts):
+        print('Simulating filling with dt = %.4f' %dt)
+        # Load the original model
+        with open(filename, 'rb') as handle:
+            col = pickle.load(handle)
+        # Clear the vessels
+        col.clearConcentrations()
+        start = time.time()
+        col.fillTree(dt = dt, tStop = 200, gif = False)
+        end = time.time()
+        col.tFillSolve(end - start)
+        print('Filling simulation took %.3f secs' %end-start)
+        # Save the model
+        folder = 'results/tConvergence/dt' + dt
+        if not os.path.exists(folder):
+            os.mkdir(folder)
+        col.saveModel(folder)
+        print('Model Saved')
+
+if __name__ == "__main__":
+    main()
